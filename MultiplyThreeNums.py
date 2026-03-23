5e416daf7b7733f2be55a463ac53da095231982f
yangnick020 <1246476813@qq.com>
2026-03-23 09:41:42 +0800

    Complete

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,7 @@
+#MutiplyThreeNums.py
+num1 = float(input("input the first number:"))
+num2 = float(input("input the second number:"))
+num3 = float(input("input the third number:"))
+product = num1 * num2 * num3
+print(f"the mutiplication is:{product}")
+

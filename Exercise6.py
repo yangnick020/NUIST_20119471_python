4e1717c0610076eac0d7c9aeef57fad117fea194
yqd <1246476813@qq.com>
2026-03-24 20:53:35 +0800

    feat: Add Exercise 6 from Lab#1

---
+++ Exercise6.py
@@ -0,0 +1,14 @@
+studentNames = ["Lisa", "Liam", "Leo", "Larry", "Linda"]
+
+print("Original list:")
+for name in studentNames:
+   print(f"{name} Evans")
+
+new_name = input("\nEnter another name to add:")
+studentNames.append(new_name)
+
+print("\nUpdated list:")
+for name in student Names:
+   print(f"{name} Evans")
+
+
